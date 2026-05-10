9f1b84a99c1fda125021a807d65f78280b10e1a7
Julian Camilo Diaz Osorno <134356840+jul1annt@users.noreply.github.com>
2026-05-10 18:56:38 -0500

    Delete Backend directory
    Here we go again

--- Backend/gateway/router.py
+++
@@ -1,6 +0,0 @@
-from fastapi import APIRouter
-from modules.data_sources.controller import router as data_sources_router
-
-api_router = APIRouter(prefix="/api")
-
-api_router.include_router(data_sources_router)
