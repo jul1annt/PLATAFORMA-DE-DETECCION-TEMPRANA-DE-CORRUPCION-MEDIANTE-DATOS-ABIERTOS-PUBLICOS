6370ca7f8f24c4beaf54e6b6ffe926fdf1cfeac9
Julian Camilo Diaz Osorno <134356840+jul1annt@users.noreply.github.com>
2026-05-13 16:37:42 -0500

    Try to Fix

--- Backend/database/migrations/env.py
+++ Backend/database/migrations/env.py
@@ -15,6 +15,7 @@ from modules.ingesta.model.SincronizacionHistorial import SincronizacionHistoria
 from modules.transformacion.model.ContratoProcesado import ContratoProcesado
 from modules.transformacion.model.ContratoAnomaloIncompleto import ContratoAnomaloIncompleto
 from modules.transformacion.model.EstadisticaCamposFaltantes import EstadisticaCamposFaltantes
+from modules.analitica.model.contrato_outlier import ContratoOutlier
 
 # Alembic Config object
 config = context.config
